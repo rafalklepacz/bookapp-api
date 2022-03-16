337fb3dce25512ca62c89661093ee893b7ed42d5
Rafał Klepacz <rafal.klepacz@hotmail.com>
2022-03-17 00:16:09 +0100

    added authentication requirement for publisher's view

--- src/webapi/views.py
+++ src/webapi/views.py
@@ -2,14 +2,17 @@ from typing import Any, Dict, List
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.db.models import Q
-from rest_framework import status
+from rest_framework import authentication, permissions, status
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from .models import Publisher, Author, Book
 from .serializers import PublisherSerializer, AuthorSerializer, BookSerializer
+from .authentication import TokenAuthentication
 
 
 class PublisherView(ViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
     def retrieve(self, request, pk=None):
         try:
             publisher = Publisher.objects.get(pk=pk)
