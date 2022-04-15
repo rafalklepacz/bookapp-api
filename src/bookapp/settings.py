30a1da5b5f76370ba6d916137fbb8311b2309307
Rafał Klepacz <rafal.klepacz@hotmail.com>
2022-04-15 13:11:14 +0200

    added STATICFILES_STORAGE value for whitenoise middleware

--- src/bookapp/settings.py
+++ src/bookapp/settings.py
@@ -175,6 +175,8 @@ MEDIA_URL = '/images/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
